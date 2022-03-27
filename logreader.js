2bb7622ffe7386abfe8fcf8474488195c3999be2
idk <hankhill19580@gmail.com>
2022-03-27 17:40:46 -0400

    make it more useful

--- logreader.js
+++ logreader.js
@@ -7,59 +7,63 @@ window.addEventListener("load", function() {
     for (var i = 0; i < lis.length; i++) {
         var li = lis[i];
         console.log("log dammit", li.textContent);
-        // create a new div with id==text of the li
-        var div = document.createElement("div");
-        div.id = li.textContent;
-
-        // request the file contents by doing an xhr
-        var xhr = new XMLHttpRequest();
-        xhr.open("GET", li.textContent.replace("#", ""), false);
-        xhr.send();
-        // put the contents of the file into the preformatted code section when a response is recieved
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState == 4) {
+        // request the file contents by doing a fetch
+        fetch(li.textContent)
+            .then(function(response) {
+                // create a new div with id==text of the li
+                var div = document.createElement("div");
+                div.id = li.textContent;
                 // create a preformatted code section inside of the div
-                console.log("xhr got log:", xhr.responseText);
                 var pre = document.createElement("pre");
-                pre.textContent = xhr.responseText;
-                // add the preformatted code section to the div
+                pre.textContent = response.text();
                 div.appendChild(pre);
-                // add the div to the body
+                // append the div to the body
                 document.body.appendChild(div);
-            }
-        }
-
-        //li.appendChild(div);
-        // add a click event to the li
-        li.addEventListener("click", function() {
-            // hide all divs
-            var divs = document.querySelectorAll("div");
-            for (var i = 0; i < divs.length; i++) {
-                divs[i].style.display = "none";
-            }
-            // show the div with the id of the li
-            document.getElementById(this.textContent).style.display = "block";
-            // scroll to the div
-            window.scrollTo(0, this.offsetTop);
-        });
-        // add exactly the same event to the a
-        var a = li.querySelector("a");
-        a.addEventListener("click", function(e) {
-            e.preventDefault();
-            // hide all divs
-            var divs = document.querySelectorAll("div");
-            for (var i = 0; i < divs.length; i++) {
-                divs[i].style.display = "none";
-            }
-            // show the div with the id of the li
-            document.getElementById(this.textContent).style.display = "block";
-            // scroll to the div
-            window.scrollTo(0, this.offsetParent.offsetTop);
-        });
+                li.addEventListener("click", function() {
+                    // hide all divs
+                    var divs = document.querySelectorAll("div");
+                    for (var i = 0; i < divs.length; i++) {
+                        divs[i].style.display = "none";
+                    }
+                    // show the div with the id of the li
+                    document.getElementById(this.textContent).style.display = "block";
+                    // scroll to the div
+                    window.scrollTo(0, this.offsetTop);
+                });
+                // add exactly the same event to the a
+                var a = li.querySelector("a");
+                a.addEventListener("click", function(e) {
+                    e.preventDefault();
+                    // hide all divs
+                    var divs = document.querySelectorAll("div");
+                    for (var i = 0; i < divs.length; i++) {
+                        divs[i].style.display = "none";
+                    }
+                    // show the div with the id of the li
+                    document.getElementById(this.textContent).style.display = "block";
+                    // scroll to the div
+                    window.scrollTo(0, this.offsetParent.offsetTop);
+                });
+            });
     }
-    // hide all divs
-    //var divs = document.querySelectorAll("div");
-    //for (var i = 0; i < divs.length; i++) {
-    //divs[i].style.display = "none";
-    //}
 });
+
+/*console.log("fetch got log:", xhr.responseText);
+var pre = document.createElement("pre");
+pre.textContent = xhr.responseText;
+// add the preformatted code section to the div
+div.appendChild(pre);
+// add the div to the body
+document.body.appendChild(div);*/
+
+
+//li.appendChild(div);
+// add a click event to the li
+
+//}
+// hide all divs
+//var divs = document.querySelectorAll("div");
+//for (var i = 0; i < divs.length; i++) {
+//divs[i].style.display = "none";
+//}
+/** */
